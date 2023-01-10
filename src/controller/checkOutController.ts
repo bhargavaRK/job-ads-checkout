5d820ac7c9006d372ea3ae48cab877007bfc20a8
bhargavaRK <kbhargava.it@gmail.com>
2023-01-10 11:37:48 +1100

    cleaned log messages

--- src/controller/checkOutController.ts
+++ src/controller/checkOutController.ts
@@ -7,10 +7,9 @@ import { checkOutRequestSchema } from '../schema/checkOutSchema'
 
 export const chekoutController = async (req: Request, res: Response) => {
   try {
-    log.info(req.body)
     const { error, value } = checkOutRequestSchema.validate(req.body, { abortEarly: false })
     if (error) res.status(400).send({ code: 'invalid request', details: error })
-    log.info(value)
+
     const customerId = value.customerId
 
     const eligibleRules = pricingRules.rules.filter((rule) => rule.customerId === customerId)
@@ -20,9 +19,8 @@ export const chekoutController = async (req: Request, res: Response) => {
     const checkoutItems = value.items as string[]
     checkoutItems.forEach((item) => checkout.add({ productId: item }))
 
-    console.log(checkout.total())
     res.status(200).send({ total: checkout.total() })
   } catch (error) {
-    log.info(`error: ${error}`)
+    log.error(`error: ${error}`)
   }
 }
